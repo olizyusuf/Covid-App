971b8230ef8f70dc2c1a2f8cef74c064367e9ac1
oliz <olizyusuf@gmail.com>
2022-07-09 22:26:02 +0700

    add gesture dector and add navigator push to detail provinsi

--- lib/widget/provinsi_item.dart
+++ lib/widget/provinsi_item.dart
@@ -1,4 +1,5 @@
 import 'package:covid_app/models/data_covid.dart';
+import 'package:covid_app/ui/detail_provinsi.dart';
 import 'package:flutter/material.dart';
 
 class ProvinsiItem extends StatelessWidget {
@@ -7,33 +8,45 @@ class ProvinsiItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(color: Colors.black12),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            dataProvinsi.key!,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailProvinsi(
+              provinsi: dataProvinsi,
             ),
           ),
-          const SizedBox(
-            height: 5,
-          ),
-          const Text('Jumlah Kasus'),
-          Text(
-            dataProvinsi.jumlah_kasus.toString(),
-            style: const TextStyle(
-              color: Colors.red,
-              fontWeight: FontWeight.bold,
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.black12),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              dataProvinsi.key!,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 5,
+            ),
+            const Text('Jumlah Kasus'),
+            Text(
+              dataProvinsi.jumlah_kasus.toString(),
+              style: const TextStyle(
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
